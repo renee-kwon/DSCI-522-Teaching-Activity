c2da96981224846b1cbb71387cacaf0e108d0861
meh2307 <mehwishbhat.ubc@gmail.com>
2022-11-23 14:30:10 -0800

    edits final

--- bar.py
+++ bar.py
@@ -1,9 +1,10 @@
+## the comments bar 
 if __name__ == "__main__":
     name = 'SAM'
     bar = 0 
     for  i in name:
     bar += ord(i)
     print(bar)
-  
+
 
 
--- foo.py
+++ foo.py
@@ -1,2 +1,7 @@
+## commennt for foo
 if __name__ == "__main__":
+   foo = list(map(
+    lambda a, b: a*b,
+    [1, 0], [0, 1]))
+    print(foo)
   print("")
